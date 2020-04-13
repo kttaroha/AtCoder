987e86bb11425a8a0f68bc6ab9090abc9d8a3e07
kt_taroha <kthamano1994@gmail.com>
2020-04-13 21:08:23 +0900

    solve ABC162A

---
+++ src/ABC1xx/ABC16x/ABC162/ABC162A.py
@@ -0,0 +1,12 @@
+def main():
+    a = input()
+    for s in a:
+        if s == "7":
+            print("Yes")
+            break
+    else:
+        print("No")
+
+
+if __name__ == "__main__":
+    main()
