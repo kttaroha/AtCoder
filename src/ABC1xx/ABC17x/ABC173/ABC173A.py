cfe5c5a95f65b6c915d4712b504cafad83e0f033
kt_taroha <kthamano1994@gmail.com>
2020-09-03 00:10:19 +0900

    solve ABC173A

--- src/ABC1xx/ABC17x/ABC173/ABC173A.py
+++ src/ABC1xx/ABC17x/ABC173/ABC173A.py
@@ -1,9 +1,12 @@
 def main():
-    t = int(input())
-    if t >= 30:
-        print("Yes")
-    else:
-        print("No")
+    M = int(input())
+    n, r = M // 1000, M % 1000
+
+    if r == 0:
+        print(0)
+        return
+
+    print((n+1)*1000-M)
 
 
 if __name__ == '__main__':
